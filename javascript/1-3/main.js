7413929d92464d68f2d2c9a27178ae831b6ce0d3
akarioyama <113684071+akarioyama@users.noreply.github.com>
2023-08-19 15:03:31 +0900

    課題提出テスト
    1-3の作成

---
+++ javascript/1-3/main.js
@@ -0,0 +1,27 @@
+//課題１//
+
+let a = 8;
+console.log(a!==10);
+
+let b = 16;
+console.log(b>=10 && b <20 && b % 2===0);
+
+
+//課題２//
+let x = 10;
+if (x>10 & x<20){
+    console.log("成功です")
+}
+else{
+    console.log("失敗です")
+};
+
+
+//課題３//
+let y = 0;
+if (y % 2 ===0){
+    console.log("偶数です")
+}
+if(y % 2 !==0){
+    console.log("奇数です") 
+};
