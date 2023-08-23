1c1f23ea801fde8184247987a6f2aa5cf92d2b65
akarioyama <113684071+akarioyama@users.noreply.github.com>
2023-08-23 21:21:42 +0900

    課題提出テスト
    1-6テスト

---
+++ javascript/1-6/1/main.js
@@ -0,0 +1,10 @@
+//課題１//
+
+let scores= [10, 15, 20, 25];
+
+console.log(scores);
+for (let i = 0; i < scores.length; i++) {
+    if (scores[i] % 2 === 0) {
+        console.log(scores[i] + "は偶数です。");
+    }
+}
---
+++ javascript/1-6/2/sub.js
@@ -0,0 +1,9 @@
+
+//課題２//
+let car={
+    gass:20.5,
+    num:1234,
+};
+console.log(car);
+console.log("ガソリンは" +car.gass + "です。");
+console.log("ガソリンは" +car.num + "です。");
