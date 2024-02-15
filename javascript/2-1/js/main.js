4d39c2bd9dff783a7fb7830b5530ab361cf5db4d
akarioyama <113684071+akarioyama@users.noreply.github.com>
2024-02-15 21:59:11 +0900

    課題提出js
    課題提出2-5

---
+++ javascript/2-1/js/main.js
@@ -0,0 +1,7 @@
+document.body.textContent = 'hello';
+console.log(window);
+
+
+let url = window.location.href;
+
+console.log(url);
---
+++ javascript/2-3/js/main.js
@@ -0,0 +1,12 @@
+// main.js
+//box要素とbtn要素を取得
+let box = document.getElementById('box');
+let btn = document.getElementById('btn');
+//ボタンがクリックされたら
+btn.onclick = function() {
+    box.style.backgroundColor = 'red';
+};
+//ボタンがクリックされたら
+btn.onclick = function() {
+    alert('ボタンが押されました');
+};
